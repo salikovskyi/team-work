dd8b3de4337170975eda906dd1a83431944181e6
rostislavvolos <volosrostislavv@gmail.com>
2022-01-14 00:47:12 +0300

    1 comm

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,42 @@
 import css from './Header.module.css'
 
 
+// const url = url('instagram.com')
+
 
 
 
 export default function Header () {
 
     return (
-        <span className={css.header_logo}>LOGO</span>
+        <header className={css.header}>
+            <span className={css.header_logo}>LOGO</span>
+
+            <nav className={css.header_nav}>
+                <ul className={css.header_list}>
+                    <li className={css.header_item}>
+                        <a href="" className={css.header_link}>
+                        Напрямки
+                        </a>
+                    </li>
+                    <li className={css.header_item}>
+                        <a href="" className={css.header_link}>
+                        Про компанію
+                        </a>
+                    </li>
+                    <li className={css.header_item}>
+                        <a href="" className={css.header_link}>
+                        Переваги
+
+                        </a>
+                    </li>
+                    <li className={css.header_item}>
+                        <a href="" className={css.header_link}>
+                        Етапи роботи
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
