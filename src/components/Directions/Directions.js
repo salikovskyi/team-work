29a78a82038d0cf833f4a38ca33c6b16525a89b2
salikovskyi <osalikovsiy@gmail.com>
2022-01-14 10:39:19 +0300

    init

--- src/components/Directions/Directions.js
+++ src/components/Directions/Directions.js
@@ -1,17 +1,17 @@
 import css from "./Directions.module.css";
-import photo1 from '../../images/descriptions/descriptions-desc/descritions1-desctop.jpg'
-import photo3 from '../../images/descriptions/descriptions-desc/descritions3-desctop.jpg'
-import photo1_2x from '../../images/descriptions/descriptions-desc/descritions1-desctop2x.jpg'
-import photo3_2x from '../../images/descriptions/descriptions-desc/descritions3-desctop2x.jpg'
-import photo1_tablet from '../../images/descriptions/descriptions-tablet/descritions1-tablet.jpg'
-import photo3_tablet from '../../images/descriptions/descriptions-tablet/descritions3-tablet.jpg'
-import photo1_tablet_2x from '../../images/descriptions/descriptions-tablet/descritions1-tablet2x.jpg'
-import photo3_tablet_2x from '../../images/descriptions/descriptions-tablet/descritions3-tablet2x.jpg'
-import photo2_mobile from '../../images/descriptions/descriptions-mobile/descriptions2-mobile.jpg'
-import photo3_mobile from '../../images/descriptions/descriptions-mobile/descriptions3-mobile.jpg'
-import photo1_mobile_2x from '../../images/descriptions/descriptions-mobile/descriptions1-mobile2x.jpg'
-import photo3_mobile_2x from '../../images/descriptions/descriptions-mobile/descriptions3-mobile2x.jpg'
-
+import photo1 from "../../images/descriptions/descriptions-desc/descritions1-desctop.jpg";
+import photo3 from "../../images/descriptions/descriptions-desc/descritions3-desctop.jpg";
+import photo1_2x from "../../images/descriptions/descriptions-desc/descritions1-desctop2x.jpg";
+import photo3_2x from "../../images/descriptions/descriptions-desc/descritions3-desctop2x.jpg";
+import photo1_tablet from "../../images/descriptions/descriptions-tablet/descritions1-tablet.jpg";
+import photo3_tablet from "../../images/descriptions/descriptions-tablet/descritions3-tablet.jpg";
+import photo1_tablet_2x from "../../images/descriptions/descriptions-tablet/descritions1-tablet2x.jpg";
+import photo3_tablet_2x from "../../images/descriptions/descriptions-tablet/descritions3-tablet2x.jpg";
+import photo2_mobile from "../../images/descriptions/descriptions-mobile/descriptions2-mobile.jpg";
+import photo3_mobile from "../../images/descriptions/descriptions-mobile/descriptions3-mobile.jpg";
+// import photo1_mobile_2x from '../../images/descriptions/descriptions-mobile/descriptions1-mobile2x.jpg'
+import photo2_mobile_2x from "../../images/descriptions/descriptions-mobile/descriptions2-mobile2x.jpg";
+import photo3_mobile_2x from "../../images/descriptions/descriptions-mobile/descriptions3-mobile2x.jpg";
 
 export default function Directions() {
   return (
@@ -22,65 +22,67 @@ export default function Directions() {
           <p className={css.directions_number}>01</p>
           <h3 className={css.directions_descr}>Дизайн інтерєру</h3>
           <picture>
-                <source
-                  srcset={`${photo1} 1x, ${photo1_2x} 2x`}
-                  media="(min-width: 1200px)"
-                />
-                <source
-                  srcset={`${photo1_tablet} 1x, ${photo1_tablet_2x} 2x`}
-                  media="(min-width: 600px)"
-                />
-                <img className={css.image}
-                  src="#"
-                  srcset={`${photo2_mobile} 1x, ${photo1_mobile_2x} 2x`}
-                  alt="pop"
-                />
-              </picture>
+            <source
+              srcset={`${photo1} 1x, ${photo1_2x} 2x`}
+              media="(min-width: 1200px)"
+            />
+            <source
+              srcset={`${photo1_tablet} 1x, ${photo1_tablet_2x} 2x`}
+              media="(min-width: 600px)"
+            />
+            <img
+              className={css.image}
+              src="#"
+              srcset={`${photo2_mobile} 1x, ${photo2_mobile_2x} 2x`}
+              alt="pop"
+            />
+          </picture>
         </li>
         <li className={css.directions_item}>
           <p className={css.directions_number}>02</p>
           <h3 className={css.directions_descr}>Виробництво меблів</h3>
           <picture>
-          <picture>
-                <source
-                  srcset={`${photo1} 1x, ${photo1_2x} 2x`}
-                  media="(min-width: 1200px)"
-                />
-                <source
-                  srcset={`${photo1_tablet} 1x, ${photo1_tablet_2x} 2x`}
-                  media="(min-width: 600px)"
-                />
-                <img className={css.image}
-                  src="#"
-                  srcset={`${photo2_mobile} 1x, ${photo1_mobile_2x} 2x`}
-                  alt="pop"
-                />
-              </picture>
+            <picture>
+              <source
+                srcset={`${photo1} 1x, ${photo1_2x} 2x`}
+                media="(min-width: 1200px)"
+              />
+              <source
+                srcset={`${photo1_tablet} 1x, ${photo1_tablet_2x} 2x`}
+                media="(min-width: 600px)"
+              />
+              <img
+                className={css.image}
+                src="#"
+                srcset={`${photo2_mobile} 1x, ${photo2_mobile_2x} 2x`}
+                alt="pop"
+              />
+            </picture>
           </picture>
         </li>
         <li className={css.directions_item}>
           <p className={css.directions_number}>03</p>
           <h3 className={css.directions_descr}>Комплексне меблювання</h3>
           <picture>
-          <picture>
-                <source
-                  srcset={`${photo3} 1x, ${photo3_2x} 2x`}
-                  media="(min-width: 1200px)"
-                />
-                <source
-                  srcset={`${photo3_tablet} 1x, ${photo3_tablet_2x} 2x`}
-                  media="(min-width: 600px)"
-                />
-                <img className={css.image}
-                  src="#"
-                  srcset={`${photo3_mobile} 1x, ${photo3_mobile_2x} 2x`}
-                  alt="pop"
-                />
-              </picture>
+            <picture>
+              <source
+                srcset={`${photo3} 1x, ${photo3_2x} 2x`}
+                media="(min-width: 1200px)"
+              />
+              <source
+                srcset={`${photo3_tablet} 1x, ${photo3_tablet_2x} 2x`}
+                media="(min-width: 600px)"
+              />
+              <img
+                className={css.image}
+                src="#"
+                srcset={`${photo3_mobile} 1x, ${photo3_mobile_2x} 2x`}
+                alt="pop"
+              />
+            </picture>
           </picture>
         </li>
       </ul>
     </div>
   );
 }
-
