480804dc8c8e7ff35bfdaa764a62694c2c258347
salikovskyi <osalikovsiy@gmail.com>
2022-01-14 22:54:20 +0300

    init

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -10,42 +10,42 @@ export default function Project() {
         </h2>
         <ul className={css.project_list}>
           <li className={css.project_item}>
-              <div className={css.project_item_div}>
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="9" cy="9" r="3" fill="#AE7B3C" />
-              <circle cx="9" cy="9" r="8.5" stroke="#AE7B3C" />
-            </svg>
+            <div className={css.project_item_div}>
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="9" cy="9" r="3" fill="#AE7B3C" />
+                <circle cx="9" cy="9" r="8.5" stroke="#AE7B3C" />
+              </svg>
 
-            <p className={css.project_item_descr}>
-              Наш колектив націлений на результат - вчасно реалізувати проект на
-              найвищому рівні. Ми забезпечуємо спокій замовника та вирішуємо
-              будь-які задачі
-            </p>
+              <p className={css.project_item_descr}>
+                Наш колектив націлений на результат - вчасно реалізувати проект
+                на найвищому рівні. Ми забезпечуємо спокій замовника та
+                вирішуємо будь-які задачі
+              </p>
             </div>
           </li>
           <li className={css.project_item}>
-              <div className={css.project_item_div}>
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="9" cy="9" r="3" fill="#AE7B3C" />
-              <circle cx="9" cy="9" r="8.5" stroke="#AE7B3C" />
-            </svg>
+            <div className={css.project_item_div}>
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="9" cy="9" r="3" fill="#AE7B3C" />
+                <circle cx="9" cy="9" r="8.5" stroke="#AE7B3C" />
+              </svg>
 
-            <p className={css.project_item_descr}>
-              Рінго найкращий підрядник прораба, дизайнера, проект-менеджера або
-              персональний помічник власника бізнесу, квартири чи салону
-            </p>
+              <p className={css.project_item_descr}>
+                Рінго найкращий підрядник прораба, дизайнера, проект-менеджера
+                або персональний помічник власника бізнесу, квартири чи салону
+              </p>
             </div>
           </li>
         </ul>
