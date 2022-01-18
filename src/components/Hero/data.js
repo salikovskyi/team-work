07e20615a018a6705407094ed7ec2e898588b4b8
salikovskyi <osalikovsiy@gmail.com>
2022-01-18 21:59:12 +0300

    popo

---
+++ src/components/Hero/data.js
@@ -0,0 +1,27 @@
+const pictures = [
+    {
+      id: 1,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      title: 'ВИРОБНИЦТВО МЕБЛІВ',
+      descr: 'Ми виконуємо комплексне виготовлення корпусних меблів для житлових ікомерційних приміщень, забезпечимо Вас якісним меблевим продуктомвласного виробництва.'
+    },
+    {
+      id: 2,
+      image:
+        'https://picsum.photos/200/300',
+      title: 'SLIDE2 SLIDE2 Slide2',
+      descr:
+        'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+    },
+    {
+      id: 3,
+      image:
+        'https://www.closetag.com/images/photo4.jpg',
+      title: 'Slide3 Slide3 Slide3',
+      descr:
+        'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+    },
+  ];
+  
+  export default pictures;
